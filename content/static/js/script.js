ced32e55fe047418c34f45cb77da2ef26fef33da
Kurtis Nusbaum <kcommiter@gmail.com>
2025-12-06 13:07:47 -0800

    Separate render and draw

--- content/static/js/script.js
+++ content/static/js/script.js
@@ -18,7 +18,7 @@ const COLORS = [
     [207, 110, 228],
     [130, 0, 128]
 ]
-const PLOT_SIZE = 100
+const PLOT_SIZE = 100;
 const UPDATE_RATE_MS = 100;
 const CANVAS_SIZE = 800;
 const MIN_SCALE = CANVAS_SIZE / PLOT_SIZE;
@@ -37,15 +37,9 @@ const ctx = canvas.getContext("2d");
 ctx.imageSmoothingEnabled = false;
 
 let scale = MIN_SCALE;
+let bitmap = null;
 
-draw_plot();
-
-const intervalId = setInterval(function() {
-    plot[random_pos()] = random_color();
-    draw_plot();
-}, UPDATE_RATE_MS);
-
-function draw_plot() {
+async function render() {
     const imgData = ctx.createImageData(PLOT_SIZE, PLOT_SIZE);
 
     for (let i = 0; i < PLOT_SIZE * PLOT_SIZE; i++) {
@@ -57,9 +51,20 @@ function draw_plot() {
         imgData.data[ipos + 3] = 255;
     }
 
-    createImageBitmap(imgData).then((rendered) => {
-        ctx.drawImage(rendered, 0, 0, PLOT_SIZE * scale, PLOT_SIZE * scale);
-    })
+    bitmap = await createImageBitmap(imgData)
+}
+
+function draw_plot() {
+    ctx.drawImage(bitmap, 0, 0, PLOT_SIZE * scale, PLOT_SIZE * scale);
+    requestAnimationFrame(draw_plot);
+}
+
+function start() {
+    draw_plot()
+    setInterval(async function() {
+        plot[random_pos()] = random_color();
+        await render();
+    }, UPDATE_RATE_MS);
 }
 
 canvas.addEventListener('wheel', function(event) {
@@ -69,5 +74,6 @@ canvas.addEventListener('wheel', function(event) {
     } else if (event.deltaY < 0) {
         scale = Math.max(scale - 0.1, MIN_SCALE);
     }
-    draw_plot();
 });
+
+render().then(start);
